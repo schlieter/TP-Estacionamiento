42e9dd9528c80c80dc2dd48a8e85d2eaaf5dcb33
schlieter <c.schlieter@hotmail.com>
2017-11-10 14:10:18 -0300

    Add files via upload

--- TP-Estacionamiento/TP BackEnd-FrontEnd/empleado.ts
+++ TP-Estacionamiento/TP BackEnd-FrontEnd/empleado.ts
@@ -1,15 +1,13 @@
 class Empleado{
     nombre:string;
     apellido: string;
-    edad:number;
     email:string;
     contraseña:string;
     turno:string;
 
-    constructor(name:string,surname: string,year:number,mail:string,pass:string, tur:string){
+    constructor(name:string,surname: string,mail:string,pass:string, tur:string){
         this.nombre = name;
         this.apellido = surname;
-        this.edad = year;
         this.email = mail;
         this.contraseña = pass;
         this.turno = tur;
